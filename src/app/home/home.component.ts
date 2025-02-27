ab6d7b8255ce2f5ca76707c93e9ebe20cc2f25b7
Raúl Báez <luarbaz@gmail.com>
2025-02-27 12:00:29 +0100

    Se añaden enlaces a redes sociales

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,17 +9,17 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   socialLinks = [
     {
-      url: 'https://github.com/tu-usuario',
+      url: 'https://github.com/liyo-dev',
       label: 'GitHub',
       icon: '/assets/icons/github.svg' 
     },
     {
-      url: 'https://linkedin.com/in/tu-usuario',
+      url: 'https://www.linkedin.com/in/liyodev',
       label: 'LinkedIn',
       icon: '/assets/icons/linkedin.svg' 
     },
     {
-      url: 'https://itch.io/tu-usuario',
+      url: 'https://liyodev.itch.io/',
       label: 'Itch.io',
       icon: '/assets/icons/itchio.svg' 
     }
