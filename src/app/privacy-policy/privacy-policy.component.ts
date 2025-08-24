0615b3df4ee90065d91147f22988259a61584fff
Raúl Báez <luarbaz@gmail.com>
2025-08-24 21:02:31 +0200

    Feat: remove footer component and related styles, add copyright notice to menu

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
@@ -9,11 +9,23 @@ import { RouterLink } from '@angular/router';
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.css'
 })
-export class PrivacyPolicyComponent {
+export class PrivacyPolicyComponent implements OnInit, OnDestroy {
   lastUpdated = '29/05/2024';
   contactEmail = 'luarbaz@gmail.com';
   developerName = 'liyodev';
   companyName = 'liyodev';
   responsibleName = 'Raúl Báez Amate';
   address = 'Pasaje Gladiolos, 37 Málaga';
+
+  ngOnInit() {
+    // Forzar scroll en el body cuando se carga el componente
+    document.body.style.overflow = 'auto';
+    document.documentElement.style.overflow = 'auto';
+  }
+
+  ngOnDestroy() {
+    // Restaurar el comportamiento original cuando se sale del componente
+    document.body.style.overflow = '';
+    document.documentElement.style.overflow = '';
+  }
 }
